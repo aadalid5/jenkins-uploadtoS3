8058d9b9447c5639ee694e99d9677a2c962e3d5d
Ariel Alejo <ariel.alejo@hbr.org>
2023-08-02 19:19:51 -0400

    fix path ../upload

--- source/uploadFilesS3.js
+++ source/uploadFilesS3.js
@@ -30,8 +30,8 @@ const cache_busted = Object.values(grunt_cache_bust.assets)
 // 3. ITERATE over files in directory and upload files to S3
 const generateCommand = (file, withCache=false)=>{
     return withCache ?
-    `aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,no-cache` :
-    `aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`
+    `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,no-cache` :
+    `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`
 }
 
 directoryFiles.forEach(file=>{
