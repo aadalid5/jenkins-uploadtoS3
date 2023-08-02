74fe7f3ceafeed12cbe1fef52a7ec4297e7c8686
Ariel Alejo <ariel.alejo@hbr.org>
2023-08-02 19:15:11 -0400

    fix errors in command S3

--- source/uploadFilesS3.js
+++ source/uploadFilesS3.js
@@ -28,19 +28,22 @@ getFilesRecursively('upload/resources')
 const cache_busted = Object.values(grunt_cache_bust.assets)
 
 // 3. ITERATE over files in directory and upload files to S3
-const uploadCommand = `aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`
-const uploadCommandWithCache = `aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,no-cache`
+const generateCommand = (file, withCache=false)=>{
+    return withCache ?
+    `aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,no-cache` :
+    `aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`
+}
 
 directoryFiles.forEach(file=>{
     if(cache_busted.includes(file)){
         // console.log(`aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,no-cache`)
         console.log('with cache', file)
-        execSync(uploadCommandWithCache)
+        execSync(generateCommand(file, true))
     }
     else {
         // console.log(`aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`)
         console.log('WithOUT cache', file)
-        execSync(uploadCommand)
+        execSync(generateCommand(file, false))
     }
 })
 
