5ff68f037a804bca8dd965542a0d1f009fc26e57
Ariel Alejo <ariel.alejo@hbr.org>
2023-08-03 10:47:58 -0400

    modify script

--- source/uploadFilesS3.js
+++ source/uploadFilesS3.js
@@ -1,47 +1,49 @@
-const fs = require("fs");
-const path = require("path");
-const execSync = require("child_process").execSync;
-let gruntCacheBustJSON;
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+const gruntCacheBustJSON = require('./resources/grunt-cache-bust.json');
 
-let resourceFiles = [];
-const getFilesInDirectory = (directory) => {
-    const filesInDirectory = fs.readdirSync(directory);
 
-    filesInDirectory.forEach(file => {
-        const filePath = path.join(directory, file);
-        if (fs.statSync(filePath).isDirectory()) {
-            getFilesInDirectory(filePath);
+const uploadToS3 = (directory) => {
+    let resourceFiles = [];
+    const getFilesInDirectory = (directory) => {
+        const filesInDirectory = fs.readdirSync(directory);
+    
+        filesInDirectory.forEach((file) => {
+            const filePath = path.join(directory, file);
+            if (fs.statSync(filePath).isDirectory()) {
+                getFilesInDirectory(filePath);
+            } else {
+                resourceFiles.push(filePath);
+            }
+        });
+    
+        resourceFiles = resourceFiles.map((file) => {
+            return file.replace('upload/', '');
+        });
+    };
+
+    const uploadAwsS3 = (file, withCacheDirective) => {
+        return withCacheDirective
+            ? `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,max-age=3153600`
+            : `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`;
+    };
+
+    // 1. Retrieve files in the directory
+    getFilesInDirectory(directory);
+
+    // 2. Get the resources that were cache busted from grunt-cache-bust.json
+    const cacheBustedResources = Object.values(gruntCacheBustJSON.assets);
+
+    // 3. Iterate over files in the directory and upload them to S3
+    resourceFiles.forEach((resource) => {
+        if (cacheBustedResources.includes(resource)) {
+            execSync(uploadAwsS3(resource, true));
         } else {
-            resourceFiles.push(filePath);
+            execSync(uploadAwsS3(resource, false));
         }
-    })
-
-    resourceFiles = resourceFiles.map(file =>{
-      return file.replace('upload/', '')
-    })
-};
-
-// 1. Retrieve files in the directory
-getFilesInDirectory('upload/resources')
-
-// 2. READ grunt-cache-bust.json
-gruntCacheBustJSON = require('./grunt-cache-bust.json');
-const cacheBustedResources = Object.values(gruntCacheBustJSON.assets)
-
-// 3. ITERATE over files in directory and upload files to S3
-const uploadAwsS3 = (file, withCacheDirective)=>{
-    return withCacheDirective ?
-    `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,max-age=3153600` :
-    `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`
+    });
 }
 
-resourceFiles.forEach(resource=>{
-    if(cacheBustedResources.includes(resource)){
-        console.log('with cache', resource)
-        execSync(uploadAwsS3(resource, true))
-    }
-    else {
-        console.log('WithOUT cache', resource)
-        execSync(uploadAwsS3(resource, false))
-    }
-})
+uploadToS3('upload/resources/css');
+uploadToS3('upload/resources/js');
