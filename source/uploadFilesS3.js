e401817b21de626bd3b9e5c650cc4c3ea2b90377
Ariel Alejo <ariel.alejo@hbr.org>
2023-08-03 15:18:57 -0400

    update script

--- source/uploadFilesS3.js
+++ source/uploadFilesS3.js
@@ -3,34 +3,35 @@ const path = require('path');
 const { execSync } = require('child_process');
 const gruntCacheBustJSON = require('./resources/grunt-cache-bust.json');
 
+const getFilesInDirectory = (directory) => {
+    let resourceFiles = [];
+    const filesInDirectory = fs.readdirSync(directory);
+
+    filesInDirectory.forEach((file) => {
+        const filePath = path.join(directory, file);
+        if (fs.statSync(filePath).isDirectory()) {
+            getFilesInDirectory(filePath);
+        } else {
+            resourceFiles.push(filePath);
+        }
+    });
+
+    resourceFiles = resourceFiles.map((file) => {
+        return file.replace('upload/', '');
+    });
+
+    return resourceFiles;
+};
+
+const uploadAwsS3 = (file, withCacheDirective) => {
+    return withCacheDirective
+        ? `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,max-age=3153600`
+        : `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`;
+};
 
 const uploadToS3 = (directory) => {
-    let resourceFiles = [];
-    const getFilesInDirectory = (directory) => {
-        const filesInDirectory = fs.readdirSync(directory);
-    
-        filesInDirectory.forEach((file) => {
-            const filePath = path.join(directory, file);
-            if (fs.statSync(filePath).isDirectory()) {
-                getFilesInDirectory(filePath);
-            } else {
-                resourceFiles.push(filePath);
-            }
-        });
-    
-        resourceFiles = resourceFiles.map((file) => {
-            return file.replace('upload/', '');
-        });
-    };
-
-    const uploadAwsS3 = (file, withCacheDirective) => {
-        return withCacheDirective
-            ? `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,max-age=3153600`
-            : `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`;
-    };
-
     // 1. Retrieve files in the directory
-    getFilesInDirectory(directory);
+    const resourceFiles = getFilesInDirectory(directory);
 
     // 2. Get the resources that were cache busted from grunt-cache-bust.json
     const cacheBustedResources = Object.values(gruntCacheBustJSON.assets);
