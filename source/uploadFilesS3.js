3b4d0afa99dd9242dba7c648e15cd0f6c28c1f04
Ariel Alejo <ariel.alejo@hbr.org>
2023-08-03 09:34:33 -0400

    update script

--- source/uploadFilesS3.js
+++ source/uploadFilesS3.js
@@ -1,51 +1,47 @@
 const fs = require("fs");
 const path = require("path");
-const grunt_cache_bust = require('./grunt-cache-bust.json');
 const execSync = require("child_process").execSync;
+let gruntCacheBustJSON;
 
-let directoryFiles = [];
-const getFilesRecursively = (directory) => {
+let resourceFiles = [];
+const getFilesInDirectory = (directory) => {
     const filesInDirectory = fs.readdirSync(directory);
-  
-    for (const file of filesInDirectory) {
-      const absolute = path.join(directory, file);
-      if (fs.statSync(absolute).isDirectory()) {
-          getFilesRecursively(absolute);
-      } else {
-          directoryFiles.push(absolute);
-      }
-    }
-    directoryFiles = directoryFiles.map(file =>{
+
+    filesInDirectory.forEach(file => {
+        const filePath = path.join(directory, file);
+        if (fs.statSync(filePath).isDirectory()) {
+            getFilesInDirectory(filePath);
+        } else {
+            resourceFiles.push(filePath);
+        }
+    })
+
+    resourceFiles = resourceFiles.map(file =>{
       return file.replace('upload/', '')
     })
 };
 
-// 1. Retrieve files in a directory
-getFilesRecursively('upload/resources')
-
+// 1. Retrieve files in the directory
+getFilesInDirectory('upload/resources')
 
 // 2. READ grunt-cache-bust.json
-const cache_busted = Object.values(grunt_cache_bust.assets)
+gruntCacheBustJSON = require('./grunt-cache-bust.json');
+const cacheBustedResources = Object.values(gruntCacheBustJSON.assets)
 
 // 3. ITERATE over files in directory and upload files to S3
-const generateCommand = (file, withCache=false)=>{
-    return withCache ?
+const uploadAwsS3 = (file, withCacheDirective)=>{
+    return withCacheDirective ?
     `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,max-age=3153600` :
     `aws s3 cp ./upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`
 }
 
-directoryFiles.forEach(file=>{
-    if(cache_busted.includes(file)){
-        // console.log(`aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file} --cache-control=public,no-cache`)
-        console.log('with cache', file)
-        execSync(generateCommand(file, true))
+resourceFiles.forEach(resource=>{
+    if(cacheBustedResources.includes(resource)){
+        console.log('with cache', resource)
+        execSync(uploadAwsS3(resource, true))
     }
     else {
-        // console.log(`aws s3 cp ../upload/${file} s3://${process.env.ASSET_BUCKET_NAME}/${file}`)
-        console.log('WithOUT cache', file)
-        execSync(generateCommand(file, false))
+        console.log('WithOUT cache', resource)
+        execSync(uploadAwsS3(resource, false))
     }
 })
-
-// const command = "aws iam list-users"
-// console.log(execSync(command).toString())
